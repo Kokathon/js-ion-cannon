14f2b76face50cc7f2655a67da3b91f7b62189d4
Oskar Risberg <kokarn@gmail.com>
2013-04-02 14:41:14 +0300

    Update request.js to include hash collision

--- request.js
+++ request.js
@@ -2,6 +2,10 @@
     Resources:
     https://hacks.mozilla.org/2009/07/cross-site-xmlhttprequest-with-cors/
     http://blog.andlabs.org/2010/12/performing-ddos-attacks-with-html5.html
+    
+    Should probably implement the well known hash table collision
+    http://1337day.com/exploit/17321
+    http://www.nruns.com/_downloads/advisory28122011.pdf
 */
 
 var url = "http://foobar.com/"
